dfa1fb855f719b9d37b60576da06790ce6ee6519
Venneti Anusha Pavani <vennetianushapavani2005@gmail.com>
2026-01-03 15:29:16 +0530

    Initial commit: ERC-20 token project

---
+++ MyToken.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyToken {
+
+    // Token details
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+
+    // Total supply
+    uint256 public totalSupply;
+
+    // Balance tracking
+    mapping(address => uint256) public balanceOf;
+
+    // Allowance: owner => (spender => amount)
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    // Constructor: runs once at deployment
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply;
+        balanceOf[msg.sender] = _initialSupply;
+    }
+
+    // Transfer tokens
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    // Approve spender
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        require(_spender != address(0), "Cannot approve zero address");
+
+        allowance[msg.sender][_spender] = _value;
+
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Transfer from on behalf of owner
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "Cannot transfer to zero address");
+        require(balanceOf[_from] >= _value, "Insufficient balance");
+        require(allowance[_from][msg.sender] >= _value, "Allowance exceeded");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
